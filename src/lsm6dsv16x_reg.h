91f134f160e4ca6579d58bb89814ecdcc4d42dea
Carlo Parata <carlo.parata@st.com>
2023-09-11 15:11:19 +0200

    Update lsm6dsv16x_reg.h
    Signed-off-by: Carlo Parata <carlo.parata@st.com>

--- src/lsm6dsv16x_reg.h
+++ src/lsm6dsv16x_reg.h
@@ -100,33 +100,6 @@ typedef struct {
 #define PROPERTY_DISABLE                (0U)
 #define PROPERTY_ENABLE                 (1U)
 
-/** @addtogroup  Interfaces_Functions
-  * @brief       This section provide a set of functions used to read and
-  *              write a generic register of the device.
-  *              MANDATORY: return 0 -> no Error.
-  * @{
-  *
-  */
-
-typedef int32_t (*lsm6dsv16x_write_ptr)(void *, uint8_t,  uint8_t *, uint16_t);
-typedef int32_t (*lsm6dsv16x_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
-typedef void (*lsm6dsv16x_mdelay_ptr)(uint32_t millisec);
-
-typedef struct {
-  /** Component mandatory fields **/
-  lsm6dsv16x_write_ptr  write_reg;
-  lsm6dsv16x_read_ptr   read_reg;
-  /** Component optional fields **/
-  lsm6dsv16x_mdelay_ptr   mdelay;
-  /** Customizable optional pointer **/
-  void *handle;
-} lsm6dsv16x_ctx_t;
-
-/**
-  * @}
-  *
-  */
-
 #endif /* MEMS_SHARED_TYPES */
 
 #ifndef MEMS_UCF_SHARED_TYPES
@@ -154,6 +127,33 @@ typedef struct {
   */
 
 #endif /* MEMS_UCF_SHARED_TYPES */
+
+/**
+  * @}
+  *
+  */
+
+/** @addtogroup  LSM6DSV16X_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device.
+  *              MANDATORY: return 0 -> no Error.
+  * @{
+  *
+  */
+
+typedef int32_t (*lsm6dsv16x_write_ptr)(void *, uint8_t,  uint8_t *, uint16_t);
+typedef int32_t (*lsm6dsv16x_read_ptr)(void *, uint8_t, uint8_t *, uint16_t);
+typedef void (*lsm6dsv16x_mdelay_ptr)(uint32_t millisec);
+
+typedef struct {
+  /** Component mandatory fields **/
+  lsm6dsv16x_write_ptr  write_reg;
+  lsm6dsv16x_read_ptr   read_reg;
+  /** Component optional fields **/
+  lsm6dsv16x_mdelay_ptr   mdelay;
+  /** Customizable optional pointer **/
+  void *handle;
+} lsm6dsv16x_ctx_t;
 
 /**
   * @}
